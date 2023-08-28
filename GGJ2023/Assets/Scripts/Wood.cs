4e16df79f9046a7ffc7c7f21ba454e77b5581804
teruya0321 <124129155+teruya0321@users.noreply.github.com>
2023-08-28 10:49:59 +0900

    Update Wood.cs

--- GGJ2023/Assets/Scripts/Wood.cs
+++ GGJ2023/Assets/Scripts/Wood.cs
@@ -2,20 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// アニメーション再生用スクリプト
 public class Wood : MonoBehaviour
 {
-    public Animator woodAnim;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void CutWood()
     {
         woodAnim.SetTrigger("Cut");
